d5f86cad6c5bc0bfcd0805cf70beaba9d040db8d
Hima Chandra Sai Vardhan Katta <kattasaivardhan838@gmail.com>
2025-05-20 11:53:07 +0530

    Create Implement Upper Bound.java

---
+++ Binary Search/BS on 1D Arrays/Implement Upper Bound.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int upperBound(int[] arr, int x) {
+        int n = arr.length;
+        int low = 0, high = n-1;
+        int ans = n;
+
+        while(low<=high){
+            int mid = low+(high-low)/2;
+
+            if(arr[mid]>x){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
