9a948645a043c5cf29bf45f51d48f98acc6b4234
Hima Chandra Sai Vardhan Katta <kattasaivardhan838@gmail.com>
2025-07-14 18:48:56 +0530

    Update RecursiveBubbleSort.java

--- Sorting/RecursiveBubbleSort.java
+++ Sorting/RecursiveBubbleSort.java
@@ -5,19 +5,13 @@ class RecursiveBubbleSort {
         if (n == 1)
             return;
 
-        int didSwap = 0;
-
         for (int j = 0; j < n - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
-                didSwap = 1;
             }
         }
-
-        if (didSwap == 0)
-            return;
 
         bubbleSort(arr, n - 1);
     }
